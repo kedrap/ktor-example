778f290b406a9b782f85f2f37ababc55e52a864c
Piotr Kedra <piotr@kraksoft.pl>
2019-01-15 23:41:33 +0100

    IntelliJ: Extract out Configuration Data

--- src/main/kotlin/BlogApp.kt
+++ src/main/kotlin/BlogApp.kt
@@ -5,10 +5,8 @@ import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 
-fun Application.module() {
+fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
     install(Routing) {
@@ -16,13 +14,4 @@ fun Application.module() {
             call.respondText("My Example Blog2", ContentType.Text.Html)
         }
     }
-}
-
-fun main(args: Array<String>) {
-    embeddedServer(
-        Netty,
-        8080,
-        watchPaths = listOf("BlogAppKt"),
-        module = Application::module
-    ).start()
 }
