6cd087fe79be73aec8686a1ac3d3cfd84c969d7a
Piotr Kedra <piotr@kraksoft.pl>
2019-01-15 23:32:31 +0100

    IntelliJ: Improve the app with the Application object

--- src/main/kotlin/BlogApp.kt
+++ src/main/kotlin/BlogApp.kt
@@ -1,18 +1,28 @@
 package blog
 
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    embeddedServer(Netty, 8080) {
-        routing {
-            get("/") {
-                call.respondText("My Example Blog", ContentType.Text.Html)
-            }
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("My Example Blog2", ContentType.Text.Html)
         }
-    }.start(wait = true)
+    }
+}
+
+fun main(args: Array<String>) {
+    embeddedServer(
+        Netty,
+        8080,
+        watchPaths = listOf("BlogAppKt"),
+        module = Application::module
+    ).start()
 }
